1493576a8d95a3daca783bc2fdca4a2b4637ee6d
Hammad Saaedi <71373419+hammadsaedi@users.noreply.github.com>
2023-10-04 00:18:57 +0500

    Time: 1 ms (23.47%), Space: 43.1 MB (31.16%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for(int i : nums){
+            if (i == 0) return 0;
+            count += (i > 0)?0:1;
+        }
+        return ((count & 1) == 0)?1:-1; 
+    }
+}
