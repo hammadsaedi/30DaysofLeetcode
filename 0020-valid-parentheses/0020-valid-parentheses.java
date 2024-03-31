83a05625906f31f0d7d0c6ea49dd415f46ca8b88
Hammad Saaedi <71373419+hammadsaedi@users.noreply.github.com>
2024-04-01 04:25:22 +0500

    Time: 1 ms (98.62%), Space: 41.5 MB (45.44%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            switch(c){
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if(stack.isEmpty() || stack.pop() != c) 
+                        return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
